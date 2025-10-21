bb04e9a419707939a811e9f3934c95bf58e14c80
Brendan  Thomas <brenthom203@gmail.com>
2025-10-21 15:24:14 -0500

    rpc.py first attempt

---
+++ rpc.py
@@ -0,0 +1,9 @@
+# Project-1.Zeus
+print("Hello user! I hope your doing wonderful!\nI wanted to play rock paper scissors and just so happened to run into you")
+choice = str(input("Do you wanna play? Yes or No."))
+if choice = Yes or yes:
+  print("Great!")
+elif choice = No or no:
+  print("Ok:/")
+else:
+  print("I cant understand you")
