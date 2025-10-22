7f6a7c07a5055e3f74c90bfad41e8f523c453dd2
Brendan  Thomas <brenthom203@gmail.com>
2025-10-21 23:00:35 -0500

    Update score_keeping.py

--- score_keeping.py
+++ score_keeping.py
@@ -1,19 +1,19 @@
 #Score keeping Test
 
-'''This is the initial count of every choice'''
+#This is the initial count of every choice
 rock = 0 
 paper = 0
 scissors = 0
 
-'''This is supposed to display the tallies'''
+#This is supposed to display the tallies
 while True:
   print("\n Current tallies: ")
   print("Rock:", rock)
   print("Paper:", paper)
   print("Scissors:", scissors)
 
-'''This is explaining to the user how to add to wach tally'''
-  user_choice = str(input("\nType out 'Rock', 'Paper, or 'Scissors' to add to each respective tally, or type 'Quit' to stop: ").lower()
+#This is explaining to the user how to add to wach tally
+  user_choice = str(input("\nType out 'Rock', 'Paper, or 'Scissors' to add to each respective tally, or type 'Quit' to stop: ")).lower()
   if user_choice == "rock":
       rock += 1
       print("You added one rock!")
@@ -23,14 +23,14 @@ while True:
   elif user_choice == "scissors":
       scissors += 1
       print("You added one scissors!")
-'''this is when the user wants to end the counting and the loop will end'''
+#this is when the user wants to end the counting and the loop will end
   elif user_choice == "quit":
       print("\nFinal Tallies: ")
       print("Rock: ", rock)
       print("Paper: ", paper)
       print("Scissors: ", scissors)
       break 
-else:
-  print("Please type 'Rock', 'Paper, 'Scissors', or 'Quit'")
+  else:
+      print("Please type 'Rock', 'Paper, 'Scissors', or 'Quit'")
           
                     
