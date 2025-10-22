75cf62b787f79e8a10d75d857102467e0e0ff2fd
Brendan  Thomas <brenthom203@gmail.com>
2025-10-21 19:38:04 -0500

    Enhance user interaction and fix typos in rpc.py
    Refactored user prompts and improved input handling for the rock-paper-scissors game.

--- rpc.py
+++ rpc.py
@@ -1,23 +1,27 @@
 # Project-1.Zeus
-print("Hello user! I hope your doing wonderful!\nI wanted to play rock paper scissors and just so happened to run into you")
-choice = str(input("Do you wanna play? Yes or No."))
+import random
 
+print("Hello user! I hope you're doing wonderful!")
+print("I wanted to play rock paper scissors and just so happened to run into you")
+
+'''Asking if the user wants to play'''
 while True:
+  choice = str(input("Do you wanna play? Yes or No: "))
   if choice.lower() == "yes":
     print("Great!")
     break
   elif choice.lower() == "no":
-    print("Ok:/")
-    break
+    print("Ok:/ Maybe next time!")
+    exit()
   else:
     print("I cant understand you. Please type 'Yes' or 'No'.")
 '''This is my first time using break and .lower in coding so It may not be the most optimized. Its supposed to loop until the user says 'yes' or 'no'.'''
 
-while play_again == "yes"
-import random
+play_again = "yes"
 options = ["rock", "paper", "scissors"]
 
-while True
+while play_again == "yes": 
+  while True:
   user_move = input("Please type either 'Rock', 'Paper', or 'scissors'.").lower()
   if user_move == "rock":
     print("You choose Rock!")
@@ -38,11 +42,16 @@ elif user_move == "paper" and computer_move == "rock":
   print("You win!")
 elif user_move == "scissors" and computer_move == "paper":
   print("You win!")
-elif user_mover == computer_move:
+elif user_move == computer_move:
   print("Its a tie!")
 else:
-  print("You loose!")
+  print("You lose!")
 
-play_again = input("Awsome!! Do you want to play again?").lower()
-if play again == "yes"
+play_again = input("\nAwsome!! Do you want to play again? (Yes or No): ").lower()
+if play again not in ["yes", "no"]:
+  print("I'll take that answer as a 'no'. Thanks for playing!")
+  break
+
+print("Goodbye!")
+
   
