bc04e5235e191ff834eca440071746c1c08fe955
Brendan  Thomas <brenthom203@gmail.com>
2025-10-21 21:08:45 -0500

    RPS.Final

--- rpc.py
+++ rpc.py
@@ -4,54 +4,46 @@ import random
 print("Hello user! I hope you're doing wonderful!")
 print("I wanted to play rock paper scissors and just so happened to run into you")
 
-'''Asking if the user wants to play'''
+# Asking if the user wants to play
 while True:
-  choice = str(input("Do you wanna play? Yes or No: "))
-  if choice.lower() == "yes":
-    print("Great!")
-    break
-  elif choice.lower() == "no":
-    print("Ok:/ Maybe next time!")
-    exit()
-  else:
-    print("I cant understand you. Please type 'Yes' or 'No'.")
-'''This is my first time using break and .lower in coding so It may not be the most optimized. Its supposed to loop until the user says 'yes' or 'no'.'''
+    choice = input("Do you wanna play? Yes or No: ").strip().lower()
+    if choice == "yes":
+        print("Great!")
+        break
+    elif choice == "no":
+        print("Ok:/ Maybe next time!")
+        exit()
+    else:
+        print("I can't understand you. Please type 'Yes' or 'No'.")
 
 play_again = "yes"
 options = ["rock", "paper", "scissors"]
 
-while play_again == "yes": 
-  while True:
-  user_move = input("Please type either 'Rock', 'Paper', or 'scissors'.").lower()
-  if user_move == "rock":
-    print("You choose Rock!")
-    break
-  elif user_move == "paper":
-    print("You choose Paper!")
-    break
-  elif user_move == "scissors":
-    print("You choose Scissors!")
-    break
-  else:
-    print("You didnt enter a valid input")
+while play_again == "yes":
+    # Get valid user move
+    while True:
+        user_move = input("Please type either 'Rock', 'Paper', or 'Scissors': ").strip().lower()
+        if user_move in options:
+            print(f"You chose {user_move.capitalize()}!")
+            break
+        else:
+            print("You didn't enter a valid input.")
+    
+    computer_move = random.choice(options)
+    print(f"The computer chose {computer_move.capitalize()}!")
 
-computer_move = random.choice(options)
-if user_move == "rock" and computer_move == "scissors":
-  print("You win!")
-elif user_move == "paper" and computer_move == "rock":
-  print("You win!")
-elif user_move == "scissors" and computer_move == "paper":
-  print("You win!")
-elif user_move == computer_move:
-  print("Its a tie!")
-else:
-  print("You lose!")
-
-play_again = input("\nAwsome!! Do you want to play again? (Yes or No): ").lower()
-if play again not in ["yes", "no"]:
-  print("I'll take that answer as a 'no'. Thanks for playing!")
-  break
+    if user_move == computer_move:
+        print("It's a tie!")
+    elif (user_move == "rock" and computer_move == "scissors") or \
+         (user_move == "paper" and computer_move == "rock") or \
+         (user_move == "scissors" and computer_move == "paper"):
+        print("You win!")
+    else:
+        print("You lose!")
+    
+    play_again = input("\nAwesome!! Do you want to play again? (Yes or No): ").strip().lower()
+    if play_again not in ["yes", "no"]:
+        print("I'll take that answer as a 'no'. Thanks for playing!")
+        break
 
 print("Goodbye!")
-
-  
