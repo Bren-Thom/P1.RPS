4919177593af9952bbea60388b5d527d0fc6af40
Brendan  Thomas <brenthom203@gmail.com>
2025-10-22 21:17:43 -0500

    random_number.py

--- random_number.py
+++ random_number.py
@@ -2,3 +2,24 @@
 
 import random
 
+one = 0
+two = 0
+three = 0
+four = 0 
+five = 0
+six = 0 
+
+def ask_to_play():
+  while True:
+    question = input("Do you wanna roll some dice with me? Yes or No: ").strip().lower():
+      if question == "yes":
+        return True
+      elif question == "no":
+        return False
+      else:
+        print("I cant understand you. Yes or No.")
+
+if not ask_to_play():
+    print("Maybe next time. Goodbye!")
+    exit()
+
